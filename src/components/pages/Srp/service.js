f8f0ca101823a9a945091eeddaccf31f8e517103
Laz2S <36720748+Laz2S@users.noreply.github.com>
2023-08-01 22:34:02 -0300

    adjust service return

--- src/components/pages/Srp/service.js
+++ src/components/pages/Srp/service.js
@@ -1,5 +1,9 @@
-export default getFacts = async () => {
-    const response = await fetch('https://catfact.ninja/fact');
-    const body = await response.json();
-    return body;
+export const getFacts = async () => {
+    try {
+        const response = await fetch('https://catfact.ninja/fact');
+        const body = await response.json();
+        return body.fact;
+    } catch (error) {
+        return error;
+    }
 }
